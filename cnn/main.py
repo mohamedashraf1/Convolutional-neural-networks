4c61eb2d8065b3fc91e693191bd2a6b6a904beb7
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2021-06-07 05:46:33 +0200

    train and evaluate model

--- cnn/main.py
+++ cnn/main.py
@@ -45,3 +45,17 @@ def generate_model():
     return model
 
 
+def train_model(model, x_train, y_train):
+    batch_size = 128
+    epochs = 15
+
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
+
+def evaluate_model(model, x_test, y_test):
+    score = model.evaluate(x_test, y_test, verbose=0)
+    print("Test loss:", score[0])
+    print("Test accuracy:", score[1])
+
