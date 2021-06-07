1b3741f1633aef944ca6ea55f095a804b30b4da0
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2021-06-07 05:47:06 +0200

    run the model and print the results

--- cnn/main.py
+++ cnn/main.py
@@ -59,3 +59,37 @@ def evaluate_model(model, x_test, y_test):
     print("Test loss:", score[0])
     print("Test accuracy:", score[1])
 
+
+# run starts at 5:36
+myModel = generate_model()
+# myModel.summary()
+(x_train, y_train), (x_test, y_test) = get_data()
+train_model(myModel, x_train, y_train)
+evaluate_model(myModel,x_test, y_test)
+# run ends at 5:44
+
+#
+# def generate_model():
+#     model = tf.keras.Sequential()
+#     # input shape:  H, W, number of channels
+#     model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+#     model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+#
+#     model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+#     model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+#
+#     model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu'))
+#     model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+#
+#     model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu'))
+#     # model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+#
+#     # model.add(tf.keras.layers.Flatten())
+#     #
+#     # model.add(tf.keras.layers.Dense(512, activation='relu'))
+#     # model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+#
+#     # model.compile()
+#     # This builds the model for the first time: model.fit(x, y, batch_size=32, epochs=10) `
+#     return model
+#
